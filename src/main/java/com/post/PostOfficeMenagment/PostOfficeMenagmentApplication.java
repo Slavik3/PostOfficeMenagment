dd460430d15c734be397836c830d0dba1b913d0f
Viacheslav.Kozlov <viacheslav.kozlov@globallogic.com>
2022-05-04 15:45:26 +0300

    done

---
+++ src/main/java/com/post/PostOfficeMenagment/PostOfficeMenagmentApplication.java
@@ -0,0 +1,13 @@
+package com.post.PostOfficeMenagment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PostOfficeMenagmentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PostOfficeMenagmentApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/post/PostOfficeMenagment/PostOfficeMenagmentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.post.PostOfficeMenagment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PostOfficeMenagmentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

84ba93469d57d01c6e1724a45a3a380a085b2ffd
Slavik3 <slavikkozlov3@gmail.com>
2022-07-26 08:21:38 +0300

    working with kafka

--- src/main/java/com/post/menagment/services/ParcelRegistrationEvent.java
+++ src/main/java/com/post/menagment/services/ParcelRegistrationEvent.java
@@ -8,7 +8,6 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 import org.apache.kafka.clients.consumer.ConsumerRecord;
 import org.apache.kafka.clients.producer.*;
-import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.kafka.core.KafkaTemplate;
@@ -29,22 +28,22 @@ public class ParcelRegistrationEvent {
     }
 
     @Transactional
-    @KafkaListener(topics = "parcelRegistrationInit")
+    @KafkaListener(topics = "parcelRegistrationInit1")
     public void consume(final ConsumerRecord<String, ParcelDTO> consumedParcel) {
         if(consumedParcel.key().equals("parcelRegistrationInitiate")) {
             Gson gson = new Gson();
             ParcelDTO parcelDTO = gson.fromJson(String.valueOf(consumedParcel.value()), ParcelDTO.class);
             Long idTo = parcelDTO.getIdTo();
             PostOffice postOffice = postOfficeService.getById(idTo);
-            ParcelRegistrationCompleted sprc = new ParcelRegistrationCompleted(parcelDTO, postOffice.getIsWorking());
-            produce(sprc);
+            ParcelRegistrationCompleted prc = new ParcelRegistrationCompleted(parcelDTO, postOffice.getIsWorking());
+            produce(prc);
         }
     }
 
     public void produce(ParcelRegistrationCompleted postOffice) {
             final String key = "parcelRegistrationCompleted";
             log.info("Producing record: {}\t{}", key, postOffice);
-            producer.send("parcelRegistration", key, postOffice).addCallback(
+            producer.send("parcelRegistration1", key, postOffice).addCallback(
                     result -> {
                         final RecordMetadata m;
                         if (result != null) {
